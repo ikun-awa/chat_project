f1042fb5ea5341c5f2a3979d0b2c33859135084e
ikun-awa <kun11451454188@gmail.com>
2025-05-22 22:26:31 -0400

    修改提交

--- src/main/java/chat/duang/formtomysql/FormController.java
+++ src/main/java/chat/duang/formtomysql/FormController.java
@@ -3,8 +3,12 @@ package chat.duang.formtomysql;
 import chat.duang.formtomysql.entity.UserMessage;
 import chat.duang.formtomysql.entity.Gender;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 public class FormController {
--- src/main/java/chat/duang/formtomysql/UserMessageRepository.java
+++ src/main/java/chat/duang/formtomysql/UserMessageRepository.java
@@ -2,6 +2,7 @@ package chat.duang.formtomysql;
 
 import chat.duang.formtomysql.entity.UserMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UserMessageRepository extends JpaRepository<UserMessage, Long> {
     Optional<UserMessage> findByUsername(String username);
