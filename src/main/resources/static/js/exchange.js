3bc5c08f1a70f75fb09a5ab9fc558cee5dc6073f
ikun-awa <kun11451454188@gmail.com>
2025-05-24 01:44:36 -0400

    大修改

--- src/main/resources/static/js/exchange.js
+++ src/main/resources/static/js/exchange.js
@@ -1,3 +1,4 @@
+/*
 //登录信息处理
 (async function () {
   'use strict';
@@ -30,7 +31,7 @@
     }, false);
   });
 })();
-
+*/
 
 
 /*
@@ -69,32 +70,7 @@
 
  */
 
-//注册信息处理
-(function () {
-  'use strict';
-  // 从整个 document 中选出所有 .needs-validation 表单
-  const forms = document.querySelectorAll('form.needs-validation');
-  forms.forEach(function (form) {
-    form.addEventListener('submit', async function (event) {
-      event.preventDefault();
-      if (!form.checkValidity()) {
-        event.stopPropagation();
-        console.log('非法表单');
-      } else {
-        $('#spin_z').show();
-        const data = new URLSearchParams(new FormData(form));
-        await fetch('/submit', { method: 'POST', body: data });
-        form.reset();
-        console.log('合法表单');
-        alert('Good boy, you hava successfully registered!');
-        setTimeout(() => {
-          window.location.assign('./lobby/lobby.html');
-        },500);
-      }
-      form.classList.add('was-validated');
-    }, false);
-  });
-})();
+
 
 /*
 (function () {
