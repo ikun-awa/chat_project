659db9977d08d76a413e69cabb768b2e661493c4
ikun-awa <kun11451454188@gmail.com>
2025-05-24 21:38:57 -0400

    登录注册修改

--- src/main/java/chat/duang/formtomysql/entity/UserMessage.java
+++ src/main/java/chat/duang/formtomysql/entity/UserMessage.java
@@ -1,30 +1,43 @@
 package chat.duang.formtomysql.entity;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 
 @Entity
 @Table(name = "user_message",
         uniqueConstraints = @UniqueConstraint(columnNames = "username"))
 public class UserMessage {
-
+    /*
+     用户名，密码：1-20
+     评价：1-100
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
+    @Size(min = 1, max = 20, message = "bad boy: LengthException, between 1-20")
     @Column(nullable = false, length = 100, unique = true)
     private String username;
 
+    @NotNull
+    @Size(min = 1, max = 20, message = "bad boy: LengthException, between 1-20")
     @Column(nullable = false, length = 255)
-    private String password;         // 原来叫 content，这里改为 password
+    private String password;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 20)
-    private Gender gender;          // 用枚举来约束值域
+    private Gender gender;
 
+    @NotBlank
     @Column(nullable = false)
     private Integer age;
 
+    @NotBlank
+    @Size(min = 1, max = 100, message = "bad boy: LengthException, between 1-100")
     @Column(columnDefinition = "TEXT")
     private String comment;
 
