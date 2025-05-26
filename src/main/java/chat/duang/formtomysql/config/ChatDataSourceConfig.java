f05e9b3e4b0c19cd7173601eae0fdcc8839d3193
ikun-awa <kun11451454188@gmail.com>
2025-05-25 20:41:54 -0400

    大厅链接

--- src/main/java/chat/duang/formtomysql/config/ChatDataSourceConfig.java
+++ src/main/java/chat/duang/formtomysql/config/ChatDataSourceConfig.java
@@ -13,43 +13,57 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import chat.duang.formtomysql.entity.chat.ChatRoom;
+import chat.duang.formtomysql.repository.chat.ChatRoomRepository;
 
 @Configuration
-// 只扫描 chat_room 相关的 Entity 和 Repository
+@EnableTransactionManagement
 @EnableJpaRepositories(
-        basePackages         = "chat.duang.formtomysql.repository",   // 这里放 ChatRoomRepository 的包
-        includeFilters       = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
-                classes = ChatRoomRepository.class),
+        basePackageClasses   = ChatRoomRepository.class,
         entityManagerFactoryRef = "chatEntityManagerFactory",
         transactionManagerRef   = "chatTransactionManager"
 )
 public class ChatDataSourceConfig {
 
-    // 读取前缀为 chat.datasource 的属性
+    /**
+     * 读取 application.properties 中前缀为 chat.datasource 的配置
+     */
     @Bean
     @ConfigurationProperties("chat.datasource")
     public DataSourceProperties chatDataSourceProperties() {
         return new DataSourceProperties();
     }
 
-    @Bean
-    public DataSource chatDataSource() {
-        return chatDataSourceProperties()
-                .initializeDataSourceBuilder()
-                .build();
+    /**
+     * 第二数据源 DataSource，名字叫 chatDataSource
+     */
+    @Bean(name = "chatDataSource")
+    public DataSource chatDataSource(
+            @Qualifier("chatDataSourceProperties") DataSourceProperties props) {
+        return props.initializeDataSourceBuilder().build();
     }
 
+    /**
+     * chat 的 EntityManagerFactory，扫描 ChatRoom 实体
+     */
     @Bean(name = "chatEntityManagerFactory")
     public LocalContainerEntityManagerFactoryBean chatEntityManagerFactory(
-            EntityManagerFactoryBuilder builder) {
+            EntityManagerFactoryBuilder builder,
+            @Qualifier("chatDataSource") DataSource ds) {
         return builder
-                .dataSource(chatDataSource())
-                .packages("chat.duang.formtomysql.entity")   // ChatRoom 实体所在包
-                .persistenceUnit("chatPU")
+                .dataSource(ds)
+                // 注意这里要指向 entity.chat 包下的 ChatRoom
+                .packages(ChatRoom.class)
+                .persistenceUnit("chat")
                 .build();
     }
 
-    @Bean
+    /**
+     * chat 的事务管理器
+     */
+    @Bean(name = "chatTransactionManager")
     public PlatformTransactionManager chatTransactionManager(
             @Qualifier("chatEntityManagerFactory") EntityManagerFactory emf) {
         return new JpaTransactionManager(emf);
--- src/main/java/chat/duang/formtomysql/repository/chat/ChatRoomRepository.java
+++ src/main/java/chat/duang/formtomysql/repository/chat/ChatRoomRepository.java
@@ -3,6 +3,4 @@ package chat.duang.formtomysql.repository.chat;
 import chat.duang.formtomysql.entity.chat.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
-    // 将来可自定义 findByXXX
-}
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> { }
