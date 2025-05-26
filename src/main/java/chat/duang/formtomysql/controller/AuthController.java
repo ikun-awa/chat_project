9bfbca79d3b85861218db448dd3377083f9c64bd
ikun-awa <kun11451454188@gmail.com>
2025-05-26 00:35:14 -0400

    更新登录状态

--- src/main/java/chat/duang/formtomysql/controller/AuthController.java
+++ src/main/java/chat/duang/formtomysql/controller/AuthController.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
 
 import java.util.Map;
 
