92bb54d5541eaa4e2a6ec30e902d5092a0ff351f
ikun-awa <kun11451454188@gmail.com>
2025-05-26 22:12:43 -0400

    登录注册修改

--- src/main/java/chat/duang/formtomysql/controller/ChatRoomRestController.java
+++ src/main/java/chat/duang/formtomysql/controller/ChatRoomRestController.java
@@ -1,6 +1,6 @@
 package chat.duang.formtomysql.controller;
 
-import chat.duang.formtomysql.entity.chat.ChatRoom;
+import chat.duang.formtomysql.model.ChatRoom;
 import chat.duang.formtomysql.repository.chat.ChatRoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
