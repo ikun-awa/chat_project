771b1f34c160b15823bab5be154ceecbbf6c181e
ikun-awa <kun11451454188@gmail.com>
2025-05-26 21:44:01 -0400

    登录注册修改

--- src/main/java/chat/duang/formtomysql/FormToMysqlApplication.java
+++ src/main/java/chat/duang/formtomysql/FormToMysqlApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication(
-        scanBasePackages = "chat.duang.formtomysql",   // 扫描所有组件、实体、仓库
+        scanBasePackages = "chat.duang.formtomysql"   // 扫描所有组件、实体、仓库
         // 如果你额外配置了多数据源，还可以用 exclude 或额外的 @EnableJpaRepositories
 )
 public class FormToMysqlApplication {
